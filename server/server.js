3044cfa3eee1636d2582009cb637b0ecdd9c62c5
nitish kumar <kumarnitish072001@gmail.com>
2022-02-27 18:53:21 +0530

    make it better

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
 require("dotenv").config();
-
+const path = require("path");
 
 // app
 const app = express();
@@ -33,5 +33,13 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
